2e442aeb63b0ab06af405cf2fde666483bd9c82f
Ankit Shrestha <74950690+Ankit-Shrestha911@users.noreply.github.com>
2021-05-17 16:19:35 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,46 @@
+const copyright = document.getElementById('copyright');
+
+function tempCalculator() {
+    const data = document.getElementById('temp').value;
+    const choose = document.getElementById("selector");
+    const temperature_Value = selector.options[choose.selectedIndex];
+    const result = document.getElementById("result_Display");
+
+    
+    if (data.length != 0) {
+        result.innerText = "Loading...";
+        if (temperature_Value.value === "celsius") {
+            const feh = Math.round((data * 9 / 5) + 32);
+        
+            setTimeout(()=>{
+                result.innerHTML = `= ${feh}°Fahrenheit`;
+            },1000);
+            
+        }
+        else {
+            const cel = Math.round((data - 32) * 5 / 9);
+            
+            setTimeout(()=>{
+                result.innerHTML = `= ${cel}°Celsius`;
+            },1000);
+
+        }
+    }
+    else {
+        alert("Please! Enter number");
+        result.innerText = "";
+    }
+
+    setTimeout(()=>{
+        copyright.innerHTML =  `&copy;Copyright 2021 Ankit Shrestha`;
+    },1100);
+    
+
+
+
+}
+
+function show(){
+    copyright.innerHTML="";
+    
+}
