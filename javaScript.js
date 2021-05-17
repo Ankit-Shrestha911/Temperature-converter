3db1f007c1ea6f45682bea325702f9fb03c25e0a
Ankit Shrestha <74950690+Ankit-Shrestha911@users.noreply.github.com>
2021-05-17 15:33:31 +0530

    Temperature-Converter

---
+++ javaScript.js
@@ -0,0 +1,32 @@
+function tempCalculator() {
+    const data = document.getElementById('temp').value;
+    const choose = document.getElementById("selector");
+    const temperature_Value = selector.options[choose.selectedIndex];
+    const result = document.getElementById("result_Display");
+
+    result.innerText = "Loading...";
+    
+    if (data.length != 0) {
+        if (temperature_Value.value === "celsius") {
+            const feh = Math.round((data * 9 / 5) + 32);
+        
+            setTimeout(()=>{
+                result.innerHTML = `= ${feh}°Fahrenheit`;
+            },1000);
+            
+        }
+        else {
+            const cel = Math.round((data - 32) * 5 / 9);
+            
+            setTimeout(()=>{
+                result.innerHTML = `= ${cel}°Celsius`;
+            },1000);
+
+        }
+    }
+    else {
+        alert("Please! Enter number");
+    }
+
+
+}
